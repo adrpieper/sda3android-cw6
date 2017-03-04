de7a7149c151982ee4f2558eb8f915e32e7a3187
Adrian <adrpieper@gmail.com>
2017-03-04 13:24:04 +0100

    Dodałem klasę Product

---
+++ src/store/Product.java
@@ -0,0 +1,40 @@
+package store;
+
+/**
+ * Created by Adrian on 2017-03-04.
+ */
+public class Product {
+    private String name;
+    private int price;
+    private Category category;
+
+    public Product(String name, int price, Category category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
