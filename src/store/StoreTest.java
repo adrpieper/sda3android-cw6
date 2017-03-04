1e452d91d12fde6bff822bc6e3c19313d9efd926
Adrian <adrpieper@gmail.com>
2017-03-04 14:11:36 +0100

    Dodałem import statyczny

--- src/store/StoreTest.java
+++ src/store/StoreTest.java
@@ -1,5 +1,7 @@
 package store;
 
+import static store.Category.*;
+
 /**
  * Created by Adrian on 2017-03-04.
  */
@@ -7,8 +9,8 @@ public class StoreTest {
 
     public static void main(String[] args) {
         Store store = new Store();
-        store.add(new Product("Masło",2,Category.FOOD));
-        store.add(new Product("Kurtka",200,Category.CLOTHES));
-        store.showProduct(Category.FOOD);
+        store.add(new Product("Masło",2, FOOD));
+        store.add(new Product("Kurtka",200, CLOTHES));
+        store.showProduct(FOOD);
     }
 }
